eebcc3eb7b0c1f8ee17cc53f0abddb7f42db9b8f
Code-Affections <codeaffections@gmail.com>
2020-05-22 19:12:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("{0} +{1 }={2} ",a,b,c);
+            Console.WriteLine("Sum="+c);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
